b935343405f020b9171b68e842d5b58dcbf03f9f
Simeon <olabs06@yahoo.com>
2022-07-26 14:55:04 +0100

    modifying components to render links

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologyList = technologies.map ((technologies) => {
+    return (
+    <span key={technologies}>{technologies}</span>
+    );
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologyList}
       </div>
     </div>
   );
